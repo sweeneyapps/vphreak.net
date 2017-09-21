6d34c6cbcf079f382c5b99e713f47a427dd7a998
Paul Sweeney Jr <paul@sweeneyapps.com>
2017-09-21 13:57:36 -0700

    Update testdom.js

--- testdom.js
+++ testdom.js
@@ -1,4 +1,8 @@
+const fs = require('fs')
+const path = require('path')
+const file = fs.readFileSync("./src/index.html", "utf8") // main index.html document
+
 module.exports = (function() {
-  // require jsdom-global/register so this can run for mocha test on ReactJS main.js
-  global.document.body.innerHTML = "<div id='root'></div>"
+    // require jsdom-global/register so this can run for mocha test on ReactJS main.js
+    document.write(file);
 })()
